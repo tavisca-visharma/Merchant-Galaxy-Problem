7b6e0693e81648888075c0378d9333966e27f67b
Vishal Sharma <vlsharma@tavisca.com>
2019-07-24 14:21:46 +0530

    Added Junit Dependencies and upgraded Java Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,11 @@ repositories {
 
 dependencies {
     testCompile("junit", "junit", "4.12")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.0")
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_10
+    targetCompatibility = JavaVersion.VERSION_1_10
 }
